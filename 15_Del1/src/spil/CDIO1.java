4779e6bf54020f00a3c13ce06e019ef9299d537d
Sofie <Sofie@Sofies_Pc>
2016-10-03 20:23:43 +0200

    bonusopgaver med.

--- 15_Del1/src/spil/CDIO1.java
+++ 15_Del1/src/spil/CDIO1.java
@@ -7,9 +7,10 @@ public class CDIO1 {
 	public static String p2;
 	public static int sumP1 = 0;
 	public static int sumP2 = 0;
-	
 	public static void main(String[] args) {
-		
+		int done = 0;
+		int six1 = 0;
+		int six2 = 0;
 		Scanner input = new Scanner(System.in);
 		Cup cup = new Cup();
 		System.out.println("****************************************");
@@ -20,6 +21,7 @@ public class CDIO1 {
 		System.out.println("Player Two input your name:");
 		p2 = input.nextLine();
 		String cp = p1;
+		while(done == 0){
 		do {
 			if(cp == p1){
 				System.out.println("Roll the die " + p1 + " (Y/N)?" );
@@ -27,10 +29,51 @@ public class CDIO1 {
 				if(ans.equals("Y")|| ans.equals("y")){
 					cup.roll();
 					System.out.println(p1 + " rolls" + cup.toString());
-					sumP1 = sumP1 + cup.getSum();
-					printPoints();
-					cp = p2;
-				} else if(ans.equals("N") || ans.equals("n")) {
+					switch(cup.getEns()) {
+					case 0: 
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						cp = p2;
+						break;
+					case 1: 
+						sumP1 = 0;
+						printPoints();
+						cp = p1;
+						break;
+					case 2: 
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						cp = p1;
+						break;
+					case 3:
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						cp = p1;
+						break;
+					case 4:
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						cp = p1;
+						break;
+					case 5:
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						cp = p1;
+						break;
+					case 6: 
+						if(six1 == 0) {
+						sumP1 = sumP1 + cup.getSum();
+						printPoints();
+						six1 = 1;
+						cp = p1;
+						} else if(six1 == 1) {
+							System.out.println(p1 + "is the winner!!!");
+							return;
+						}
+						break;
+					}
+					} 
+				 else if(ans.equals("N") || ans.equals("n")) {
 					System.out.println("Game over");
 					break;
 				} else {
@@ -42,9 +85,49 @@ public class CDIO1 {
 				if(ans.equals("Y")|| ans.equals("y")){
 					cup.roll();
 					System.out.println(p2 + " rolls" + cup.toString());
-					sumP2 = sumP2 + cup.getSum();
-					printPoints();
-					cp = p1;
+					switch(cup.getEns()) {
+					case 0: 
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						cp = p1;
+						break;
+					case 1: 
+						sumP2 = 0;
+						printPoints();
+						cp = p2;
+						break;
+					case 2: 
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						cp = p2;
+						break;
+					case 3:
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						cp = p2;
+						break;
+					case 4:
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						cp = p2;
+						break;
+					case 5:
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						cp = p2;
+						break;
+					case 6: 
+						if(six2 == 0) {
+						sumP2 = sumP2 + cup.getSum();
+						printPoints();
+						six2 = 1;
+						cp = p2;
+						} else if(six2 == 1) {
+							System.out.println(p2 + "is the winnersdfg!!!");
+							return;
+						}
+						break;
+					}
 				} else if (ans.equals("N")|| ans.equals("n")) {
 					System.out.println("Game over");
 					break;
@@ -54,17 +137,26 @@ public class CDIO1 {
 			}
 		}
 		while(sumP1 < 40 && sumP2 < 40); 
-			if(sumP1 > sumP2) {
-				System.out.println(p1 + " is the winner!!!");
-			}	else if(sumP2 > sumP1){
-				System.out.println(p2 + " is the winner!!!");
+			if(cp == p2){
+				if(cup.getEns() == 2 || cup.getEns() == 3 || cup.getEns() == 4|| cup.getEns() == 5|| cup.getEns() == 6){
+					System.out.println(p2 + " is the winner!!!");
+					done = 1;
+				} else {
+					cp = p2;
+				}
+			} else if(cp == p1) {
+				if(cup.getEns() == 2 || cup.getEns() == 3 || cup.getEns() == 4|| cup.getEns() == 5|| cup.getEns() == 6){
+					System.out.println(p1 + " is the winner!!!");
+					done = 1;
+				} else {
+					cp = p1;
+				}
 			}
-			input.close();
+		}
+		input.close();
 	}
-
-public static void printPoints(){
+public static void printPoints() {
 	System.out.println(p1 + " has" + sumP1 + " points");
 	System.out.println(p2 + " has" + sumP2 + " points");
-	}
-
+}
 }
