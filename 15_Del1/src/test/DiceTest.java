e2829c44c0000e57a7510372c18fe63df24604b1
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-10-04 16:36:38 +0200

    Added Dice test

--- 15_Del1/src/test/DiceTest.java
+++ 15_Del1/src/test/DiceTest.java
@@ -4,12 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import spil.Dice;
+
 public class DiceTest {
 
-	@Test
+	@Test 
 	public void test() {
-		
-		fail("Not yet implemented");
+		int en=0,to=0,tre=0,fire=0,fem=0,seks=0;
+		Dice d1 = new Dice();
+		for(int i=0;i<10000;i++){
+			d1.roll();
+			switch(d1.getValue()){
+			case 1:
+				en++;
+				break;
+			case 2:
+				to++;
+				break;
+			case 3:
+				tre++;
+				break;
+			case 4:
+				fire++;
+				break;
+			case 5:
+				fem++;
+				break;
+			case 6:
+				seks++;
+				break;
+			}
+		}
+		System.out.println(en+"\n"+to+"\n"+tre+"\n"+fire+"\n"+fem+"\n"+seks);
 	}
 
 }
+
